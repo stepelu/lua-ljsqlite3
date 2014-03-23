444076070ad354889178a361fa03ba725a173312
Stefano Peluchetti <phd.st.p@gmail.com>
2014-03-23 15:46:31 +0000

    Initial public commit.

---
+++ init.lua
@@ -0,0 +1,12 @@
+--------------------------------------------------------------------------------
+-- Forwarder for ljsqlite3 module.
+--
+-- Copyright (C) 2011-2014 Stefano Peluchetti. All rights reserved.
+--
+-- Features, documentation and more: http://www.scilua.org .
+-- 
+-- This file is part of the LJSQLite3 library, which is released under the MIT 
+-- license: full text in file LICENSE.TXT in the library's root folder.
+--------------------------------------------------------------------------------
+
+return require "ljsqlite3._ljsqlite3"
