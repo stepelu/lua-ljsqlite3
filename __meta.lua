e3aedb0cf71b6dd494b09fda7f910be5586593bc
Stefano Peluchetti <phd.st.p@gmail.com>
2014-10-20 12:46:46 +0200

    Added __meta.lua

---
+++ __meta.lua
@@ -0,0 +1,17 @@
+--------------------------------------------------------------------------------
+-- Metadata for LJSQLite3.
+-- 
+-- Copyright (C) 2011-2014 Stefano Peluchetti. All rights reserved.
+--
+-- Features, documentation and more: http://www.scilua.org .
+-- 
+-- This file is part of the LJSQLite3 library, which is released under the MIT 
+-- license: full text in file LICENSE.TXT in the library's root folder.
+--------------------------------------------------------------------------------
+
+return {
+  version = "1.0.0",
+  require = {
+    xsys = "1.0",
+  }
+}
