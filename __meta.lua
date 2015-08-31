224b6d18b9200dd191f03fac9c498a61338be4b0
Stefano Peluchetti <phd.st.p@gmail.com>
2015-08-31 22:44:58 +0100

    Update __meta.lua

--- __meta.lua
+++ __meta.lua
@@ -1,17 +1,11 @@
---------------------------------------------------------------------------------
--- Metadata for LJSQLite3.
--- 
--- Copyright (C) 2011-2014 Stefano Peluchetti. All rights reserved.
---
--- Features, documentation and more: http://www.scilua.org .
--- 
--- This file is part of the LJSQLite3 library, which is released under the MIT 
--- license: full text in file LICENSE.TXT in the library's root folder.
---------------------------------------------------------------------------------
-
 return {
-  version = "1.0.0",
+  version = "1.0.1",
   require = {
-    xsys = "1.0",
-  }
+    clib_sqlite3 = "3",
+    luajit = "2.0",
+    xsys   = "1.0",
+  },
+  license = "MIT <http://opensource.org/licenses/MIT>",
+  homepage = "http://scilua.org/ljsqlite3.html",
+  description = "LJSQLITE3 - LJSQlite Interface",
 }
