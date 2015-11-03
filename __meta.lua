42cc61cbacb9837c19c0def009f3cfe5dd215a3f
Stefano Peluchetti <phd.st.p@gmail.com>
2015-11-03 19:26:27 +0000

    Prepare for v1.0.2.

--- __meta.lua
+++ __meta.lua
@@ -1,11 +1,13 @@
 return {
-  version = "1.0.1",
+  name = "ljsqlite3",
+  version = "1.0.2",
   require = {
+    luajit = "2.0",
     clib_sqlite3 = "3",
     luajit = "2.0",
     xsys   = "1.0",
   },
   license = "MIT <http://opensource.org/licenses/MIT>",
   homepage = "http://scilua.org/ljsqlite3.html",
-  description = "LJSQLITE3 - LJSQlite Interface",
+  description = "SQlite3 interface",
 }
